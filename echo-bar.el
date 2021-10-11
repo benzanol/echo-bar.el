1745995e2ec3fdd5276e702a77db3faf92390ea4
Adam Tillou <qaiviq@gmail.com>
2021-10-11 00:39:28 -0400

    Trigger displaying after any message is sent

--- echo-bar.el
+++ echo-bar.el
@@ -4,45 +4,60 @@
 (defvar echo-bar-max-message-length 300
   "Maximum length of message to display at the left side of the echo area")
 
+(add-hook 'post-command-hook 'echo-bar-display)
+(advice-add 'message :after 'echo-bar-display)
+
 (defun echo-bar-display (&rest args)
   "Compile and display the echo bar text"
   (interactive)
-  (ignore-errors
+  (unless echo-bar--inhibit
     (let* ((message-log-max nil)
            (inhibit-read-only t)
-           (previous-output (with-current-buffer " *Echo Area 1*" (buffer-string)))
+           (echo-bar--inhibit t)
+           (echo-0 (with-current-buffer " *Echo Area 0*" (buffer-string)))
+
+           (previous-output
+            (with-current-buffer " *Echo Area 1*"
+              (push echo-0 echo0s)
+              (unless (or (string= echo-0 "") (string= (substring echo-0 0 1) "§"))
+                (delete-region (point-min) (point-max)) (insert echo-0))
+              (when (and (> (point-max) 1) (string= (buffer-substring 1 2) "§"))
+                (delete-region (point-min) (point-max)))
+              (buffer-string)))
+
            ;; The right aligned text
            (text (format "%s  |  %s  |  %s"
                          (echo-bar-battery-format)
                          (format-time-string "  %b %d")
                          (format-time-string "  %H:%M")))
+
            ;; The width in pixels of the right aligned text
            (text-width (save-window-excursion
                          (when (minibufferp nil) (other-window 1))
                          (delete-other-windows)
                          (with-temp-buffer
-                           (switch-to-buffer " *temp*")
-                           (insert text)
-
-                           ;; Add a bit of padding, so that the ] at the end of the echo in the minibuffer
-                           ;; doesn't wrap to the next line
-                           (insert "  ")
-
-                           ;; Get the pixel width of the right align text, in case the default font is variable pitch
+                           (switch-to-buffer (current-buffer))
+                           (insert (concat text "  "))
                            (car (window-text-pixel-size (selected-window) (point-min) (point-max))))))
+
            ;; The pixel column to align the right aligned text
            (align-column (- (frame-text-width) text-width))
            ;; Space with display property to align the right aligned text to the correct position
            (align-space (propertize "§" 'display `(space . (:align-to (,align-column)))))
+           ;; Indicates that a particular message is from the echo bar
+           (indicator (propertize "§" 'invisible t))
            ;; Makes the echo area taller
            (tall-space (propertize " " 'display '((height 1.5) (raise -0.1)))))
+      (setq mywid text-width)
+      (setq mycol align-column)
 
       (if (minibufferp nil)
-          (progn (message "%s%s" align-space text)
+          (progn (message "%s%s%s" indicator align-space text)
                  (setq-local truncate-lines nil))
-        (message "%s%s%s%s" previous-output tall-space align-space text)))))
+        (message "%s%s%s%s%s" indicator previous-output tall-space align-space text)))))
 
 (defun echo-bar-battery-format ()
+  "Return a string containing a formatted battery display for the echo bar"
   (let* ((status (funcall battery-status-function))
          (percent (round (string-to-number (battery-format "%p" status))))
          (power-method (battery-format "%L" status)))
