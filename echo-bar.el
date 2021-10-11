1aeab165ebd5e7cd27e42858b0d998899eb1b449
Adam Tillou <qaiviq@gmail.com>
2021-10-11 12:05:56 -0400

    Remove pixelwise calculations to increase speed

--- echo-bar.el
+++ echo-bar.el
@@ -1,8 +1,8 @@
 (defvar echo-bar--inhibit nil
   "Inhibit displaying the echo bar to avoid recursive loops")
 
-(defvar echo-bar-center-padding 100
-  "Minimum width in pixels between the left and right aligned text")
+(defvar echo-bar-center-padding 10
+  "Minimum number of columns between the left and right aligned text")
 
 (defun echo-bar-display (&rest args)
   "Compile and display the echo bar text"
@@ -16,55 +16,36 @@
            (echo-0 (with-current-buffer " *Echo Area 0*" (buffer-string)))
 
            ;; The right aligned text
-           (text (funcall echo-bar-function))
+           (right-text (funcall echo-bar-function))
+
+           (align-position (- (window-text-width (cadr (window-tree))) (length right-text)))
+           (align-space (propertize "§" 'display `(space :align-to ,(- align-position 2))))
 
            ;; The text that would otherwise be displayed in the minibuffer
            (previous-echo
             (with-current-buffer " *Echo Area 1*"
-              (push echo-0 echo0s)
               (unless (or (string= echo-0 "") (string= (substring echo-0 0 1) "§"))
                 (delete-region (point-min) (point-max)) (insert echo-0))
               (when (and (> (point-max) 1) (string= (buffer-substring 1 2) "§"))
                 (delete-region (point-min) (point-max)))
               (buffer-string)))
 
+           (left-max-width (- (window-text-width (cadr (window-tree)))
+                              (length right-text) echo-bar-center-padding))
+
+           (left-text (substring previous-echo 0 (min (length previous-echo) left-max-width)))
+
            ;; Indicates that a particular message is from the echo bar
            (indicator (propertize "§" 'invisible t))
 
            ;; Makes the echo area taller
-           (tall-space (propertize " " 'display '((height 1.5) (raise -0.1))))
+           (tall-space (propertize " " 'display '((height 1.5) (raise -0.1)))))
 
-           right-width right-column align-space
-           left-width left-length left-text y start-x end-x)
-
-      ;; Calculate the pixel lengthts of the left and right aligned text
-      (save-window-excursion
-        (when (minibufferp nil) (other-window 1))
-        (delete-other-windows)
-        (with-temp-buffer
-          (switch-to-buffer (current-buffer))
-          ;; Calculate the width of the right aligned text
-          (insert (concat text "  "))
-          (setq right-width (car (window-text-pixel-size nil (point-min) (point-max)))
-                right-column (- (frame-text-width) right-width)
-                align-space (propertize "§" 'display `(space . (:align-to (,right-column)))))
-
-          ;; Calculated the length of the left aligned text
-          (setq left-width (- (frame-text-width) right-width echo-bar-center-padding))
-          (if (string= previous-echo "")
-              (setq left-text "")
-            (delete-region (point-min) (point-max))
-            (setq start-x (car (posn-x-y (event-start nil)))
-                  y (cdr (posn-x-y (event-start nil))))
-            (insert previous-echo)
-            (beginning-of-line)
-            (setq left-length (- (cadr (posn-at-x-y (+ start-x left-width) y)) 4))
-            (setq left-text (concat (buffer-substring (point-min) left-length) "...")))))
 
       (if (minibufferp nil)
-          (progn (message "%s%s%s" indicator align-space text)
+          (progn (message "%s%s%s" indicator align-space right-text)
                  (setq-local truncate-lines nil))
-        (message "%s%s%s%s%s" indicator left-text tall-space align-space text)))))
+        (message "%s%s%s%s%s" indicator left-text tall-space align-space right-text)))))
 
 
 (add-hook 'post-command-hook 'echo-bar-display)
