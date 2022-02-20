096eca53a9bb85ce944dab4abbf42302de6710b4
Adam Tillou <qaiviq@gmail.com>
2022-02-19 22:57:43 -0500

    Add package header and footer

--- echo-bar.el
+++ echo-bar.el
@@ -1,3 +1,36 @@
+;;; echo-bar.el --- Turn the echo area into a custom status bar  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021-2022  Adam Tillou
+
+;; Author: Adam Tillou <qaiviq@gmail.com>
+;; Keywords: tools
+;; Version: 1.0.0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package will allow you to display a custom status at the end
+;; of the echo area, like polybar but inside of emacs.
+
+;; To install, just run `M-x package-install RET echo-bar RET`.
+;; To customize the text that gets displayed, set the variable
+;; `echo-bar-function` to the name of your own custom function.
+;; To turn the echo bar on or off, use `echo-bar-mode`.
+
+;;; Code:
+
 (defgroup echo-bar nil
   "Display text at the end of the echo area."
   :group 'applications)
@@ -7,7 +40,7 @@
   :group 'echo-bar
   :type 'number)
 
-(defcustom echo-bar-function 'echo-bar-default-function
+(defcustom echo-bar-function #'echo-bar-default-function
   "Function that returns the text displayed in the echo bar."
   :group 'echo-bar
   :type 'function)
@@ -115,3 +148,6 @@ If nil, don't update the echo bar automatically."
   "Default value of `echo-bar-function`.
 Displays the date and time in a basic format."
   (format-time-string "%b %d | %H:%M:%S"))
+
+(provide 'echo-bar)
+;;; echo-bar.el ends here
