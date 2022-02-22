73f2301e04dec376f44d214b3e74c98f1975c650
Adam Tillou <qaiviq@gmail.com>
2022-02-21 21:01:13 -0500

    Capitalize Emacs in description

--- echo-bar.el
+++ echo-bar.el
@@ -23,7 +23,7 @@
 ;;; Commentary:
 
 ;; This package will allow you to display a custom status at the end
-;; of the echo area, like polybar but inside of emacs.
+;; of the echo area, like polybar but inside of Emacs.
 
 ;; To install, just run `M-x package-install RET echo-bar RET`.
 ;; To customize the text that gets displayed, set the variable
